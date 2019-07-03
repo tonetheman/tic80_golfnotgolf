9012d1101b59dc140ca8e59918f52c511bb21796
tone.chromebook <tonetheman@gmail.com>
2019-07-03 07:06:16 -0500

    fixed stupid typo at the top

--- split_index.py
+++ split_index.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-""""
+"""
 thought here is make it easy to break up the generated index.html from tic80
 into something that is easier to deal with for js
 no real reason
 and the regex stuff is not working
-""""
+"""
 
 import re
 
-SCRIPT_PAT = re.compile("<script type=\"text/javascript\">", re.MULTILINE)
+SCRIPT_PAT = re.compile("<script type=\"text/javascript\">(.*?)</script>", re.DOTALL)
 
 inf = open("index.html","r")
 data = inf.read()
@@ -17,5 +17,7 @@ inf.close()
 
 print("len of data",len(data))
 res = SCRIPT_PAT.findall(data)
-print(res)
+print(len(res))
+print("len of res[0]", len(res[0]))
+print(res[0][0:80])
 
